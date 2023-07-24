54d61a31bc27818c4675e5422131b371b7d8bd35
fistum1 <fistumitiku@gmail.com>
2023-07-24 18:47:53 +0300

    Sorting Algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - It sorts array lements from min to max value
+ * @array: An array
+ * @size: An array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+	size_t a, index, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (a = 0; a < size; a++)
+		for (index = 0; index < size; index++)
+		{
+			if (array[index] > array[index + 1] && array[index + 1])
+			{
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
+			}
+		}
+}
